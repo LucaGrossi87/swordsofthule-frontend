2729598e763b9ef1a96ce42f4a3fd122fd546150
Luca Grossi <lucagrossi87@gmail.com>
2024-09-09 10:57:55 +0200

    swordsofthule_partyCombat

--- swordsofthule/src/app/app.component.ts
+++ swordsofthule/src/app/app.component.ts
@@ -51,60 +51,73 @@ ngOnInit(): void {
 }
 
 combat() {
-  if (this.selectedHero!=undefined && this.selectedMonster) {
+  if (this.partyArray.length>0 && this.selectedMonster) {
     this.isCombat=true
-    for (let i = 0; i < this.selectedHero.itemList.length; i++) {
-      const item = this.selectedHero.itemList[i];
-      this.attBonus+=item.attack
-      this.defBonus+=item.defence
-    }
-    while (this.selectedHero.hitPoints>0 && this.selectedMonster.hitPoints>0) {
-      this.selectedMonster.hitPoints-=Math.max(0,(this.selectedHero.attack+this.attBonus+Math.floor(Math.random() * 7)) - (this.selectedMonster.defence+Math.floor(Math.random() * 7)))
-      console.log(this.selectedMonster.hitPoints);
-      if (this.selectedMonster.hitPoints>0) {
-        console.log(this.selectedHero.hitPoints);
-        this.selectedHero.hitPoints-=Math.max(0, (this.selectedMonster.attack+Math.floor(Math.random() * 7)) - (this.selectedHero.defence+this.defBonus+Math.floor(Math.random() * 7)))
-        console.log(this.selectedHero.hitPoints);
+    while (this.partyArray.length>0 && this.selectedMonster.hitPoints>0) {
+      for (let h = 0; h < this.partyArray.length; h++) {
+        const hero = this.partyArray[h];
+        for (let i = 0; i < hero.itemList.length; i++) {
+          const item = hero.itemList[i];
+          this.attBonus+=item.attack
+          this.defBonus+=item.defence
+        }
+        if (hero.hitPoints>0 && this.selectedMonster.hitPoints>0) {
+          this.selectedMonster.hitPoints-=Math.max(0,(hero.attack+this.attBonus+Math.floor(Math.random() * 7)) - (this.selectedMonster.defence+Math.floor(Math.random() * 7)))
+          console.log(this.selectedMonster.hitPoints);
+        } else if (hero.hitPoints<= 0) {
+          hero.hitPoints=0
+          delete this.partyArray[h]
+          this.combatExit+=`${hero.name} è morto`
+        }
       }
+        if (this.selectedMonster.hitPoints>0) {
+          for (let h = 0; h < this.partyArray.length; h++) {
+            const hero = this.partyArray[h]
+            hero.hitPoints-=Math.max(0, (this.selectedMonster.attack+Math.floor(Math.random() * 7)) - (hero.defence+this.defBonus+Math.floor(Math.random() * 7)))
+            console.log(hero.name + hero.hitPoints);
+          }
+        } else {
+          this.selectedMonster.hitPoints=0
+          this.xpLoot=this.selectedMonster.level*100
+          this.shardsLoot=Math.floor((this.selectedMonster.level*0.75)*10+(this.selectedMonster.level*Math.floor(Math.random()*6)))
+          for (let h = 0; h < this.partyArray.length; h++) {
+            const hero = this.partyArray[h];
+            hero.user.goldShards+=this.shardsLoot
+            hero.xp+=this.xpLoot
+            for (let i = 0; i < 20; i++) {
+              if (hero.xp>=Math.pow(2,i)*1000) {
+                this.levelUp=i+2
+                hero.level=this.levelUp}
+            }
+          }
+          this.combatExit=`${this.selectedMonster.name} è morto`
+        }
     }
-    if(this.selectedHero.hitPoints <= 0) {
-      this.selectedHero.hitPoints=0
-      this.combatExit=`${this.selectedHero.name} è morto`
-    } else {
-      this.selectedMonster.hitPoints=0
-      this.xpLoot=this.selectedMonster.level*100
-      this.shardsLoot=Math.floor((this.selectedMonster.level*0.75)*10+(this.selectedMonster.level*Math.floor(Math.random()*6)))
-      this.selectedHero.user.goldShards+=this.shardsLoot
-      this.selectedHero.xp+=this.xpLoot
-      for (let i = 0; i < 20; i++) {
-        if (this.selectedHero.xp>=Math.pow(2,i)*1000) {
-          this.levelUp=i+2
-          this.selectedHero.level=this.levelUp}
-      }
-      this.combatExit=`${this.selectedMonster.name} è morto`
     }
 
-    console.log(this.selectedHero);
+    for (let h = 0; h < this.partyArray.length; h++) {
+      const hero = this.partyArray[h];
+      this.appSvc.updateHero(hero.id!, hero).subscribe({
+        next: () => console.log('Update successful'),
+        error: (err) => {
+          console.error('Update failed', err);
+          console.log(this.selectedHero);
+          alert(`Error: ${err.message || 'Unknown error'}`);
+        }
+      });
+      this.appSvc.updateUser(hero.user.id!, hero.user).subscribe({
+        next: () => console.log('Update successful'),
+        error: (err) => {
+          console.error('Update failed', err);
+          alert(`Error: ${err.message || 'Unknown error'}`);
+        }
+      });
+    }
 
-    this.appSvc.updateHero(this.selectedHero.id!, this.selectedHero).subscribe({
-      next: () => console.log('Update successful'),
-      error: (err) => {
-        console.error('Update failed', err);
-        console.log(this.selectedHero);
-        alert(`Error: ${err.message || 'Unknown error'}`);
-      }
-    });
-
-    this.appSvc.updateUser(this.selectedHero.user.id!, this.selectedHero.user).subscribe({
-      next: () => console.log('Update successful'),
-      error: (err) => {
-        console.error('Update failed', err);
-        alert(`Error: ${err.message || 'Unknown error'}`);
-      }
-    });
+    this.partyArray=[]
 
   }
-}
+
 
 checkHero(event: any, hero: Hero) {
   if (event.target.checked) {
